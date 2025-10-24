6a0b62942dc80f0ee75a37e0c64b51a0cfc62f0a
Emily-060503 <emilyquinonesgamiz1@gmail.com>
2025-10-24 09:18:26 -0600

    Actualizacion base de datos

--- index.js
+++ index.js
@@ -7,102 +7,129 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 
 //express
-const admin = require("firebase-admin");
-const cors = require("cors");
+const admin = require('firebase-admin');
+const cors = require('cors');
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-/*
-const serviceAccount = require("./firebase_key.json");
+//credenciales
+/*const serviceAccount = require('./firebase_key.json');
+const e = require('express');
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
+const db = admin.firestore(); // Rutas de la API de productos
 */
+
 let serviceAccount;
 
-if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
-  // Si está en Render, se carga desde la variable
-  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+if(process.env.FIREBASE_SERVICE_ACCOUNT_KEY){
+    // Si está en Render, se carga  desde la variable
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 } else {
-  // Si estás en local, se usa el archivo
-  serviceAccount = require('./firebase_key.json');
+    // Si está en local, se usa le archivo
+    serviceAccount = require('./firebase_key.json');
 }
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount)
 });
 
 
 const db = admin.firestore();// Rutas de la API de productos
- 
 
 
-app.get("/productos", async (req, res) => {
-  try {
-    const items = await db.collection("productos").get();
-    const productos = items.docs.map(doc => {
-      const data = doc.data();
-      return {
-        id: doc.id,
-        nombre: data.nombre,
-        descripcion: data.descripcion,
-        precio: data.precio,
-        imagen: data.imagen
-      };
-    });
-    res.render('inicio', { productos });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+// Ruta para mostrar todos los productos
+app.get('/productos', async (req, res) => {
+    try {
+        const items = await db.collection('productos').get();
+        const productos = items.docs.map(doc => {
+            const data = doc.data();
+            return {
+                id: doc.id,
+                nombre: data.nombre,
+                descripcion: data.descripcion,
+                precio: data.precio,
+                imagen: data.imagen
+            };
+        });
+        res.render('inicio', { productos });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
+
 
 app.get('/productos/add', (req, res) => {
-res.render('form', { producto: null, nombre: 'Crear Producto' });
-});
-app.post('/productos', async (req, res) => {
-try {
-    const { nombre, precio, descripcion, imagen } = req.body;
-    const nuevo = {
-    nombre: nombre || '',
-    precio: parseFloat(precio) || 0,
-    descripcion,
-    imagen: imagen || ''
-};
-await db.collection('productos').add(nuevo);
-res.redirect('/productos');
-} catch (err) {
-    console.error(err);
-    res.status(500).send('Error al crear producto');
-}
+    res.render('form', { productos: null, nombre: 'Crear Producto' });
 });
 
-
-
- app.get('/productos1', (req, res) => {
-  const productos = [
-    {
-      nombre: 'Audífonos Bluetooth',
-      descripcion: 'Sonido de alta calidad y cancelación de ruido.',
-      precio: 899.99,
-      imagen: 'https://picsum.photos/200'
-    },
-    {
-      nombre: 'Smartwatch Pro',
-      descripcion: 'Controla tu salud y recibe notificaciones.',
-      precio: 1299.50,
-      imagen: 'https://picsum.photos/200?2'
-    },
-    {
-      nombre: 'Cámara 4K',
-      descripcion: 'Captura tus momentos con resolución ultra HD.',
-      precio: 4999.00,
-      imagen: 'https://picsum.photos/200?3'
+app.post('/productos', async(req, res) => {
+  try {
+      const { nombre, descripcion, precio, imagen } = req.body;
+      const nuevo = {
+        nombre: nombre || '',
+        precio: parseFloat(precio) || 0,
+        descripcion,
+        imagen: imagen || ''
+      };
+      await db.collection('productos').add(nuevo);
+      res.redirect('/productos');
+    }catch (err) {
+        console.error(err);
+        res.status(500).send({ error: err.message });
     }
-  ];
-  res.render('inicio', { productos });
 });
+            
+app.get('/productos1', (req, res) => {
 
+    const productos = [
+        {
+            nombre: 'Jale Real',
+            descripcion: 'Crema hidratante para rostro, anti vejes',
+            precio: 930,
+            imagen: 'https://picsum.photos/200?1'
+        },
+        {
+            nombre: 'Aceite de Almendras',
+            descripcion: 'Aceite corporal nutritivo antiestrias',
+            precio: 500,
+            imagen: 'pictues/aceite.png'
+        },
+        {
+            nombre: 'Labial Always',
+            descripcion: 'Labial infalible de larga duración',
+            precio: 250,
+            imagen: 'pictures/labial.jpg'
+        },
+        {
+            nombre: 'Perfume Double',
+            descripcion: 'Diablitos de mujer con aromas frescos y juveniles',
+            precio: 260,
+            imagen: 'pictures/double.png'
+        },
+        {
+            nombre: 'Perfume Navigo',
+            descripcion: 'Perfume de hombre con fragancia fresca y duradera',
+            precio: 550,
+            imagen: 'pictures/navigo.png'
+        },
+        {
+            nombre: 'Perfume para bebe',
+            descripcion: 'Perfume suave y delicado para la piel sensible de los bebes',
+            precio: 250,
+            imagen: 'pictures/bebe.png'
+        }
+   ];
+   // res.render('inicio', { productos: productos});
+   //console.log(productos);
+   res.render('inicio',{
+    //titulo: 'Pagina de inico',
+    //mensaje: 'Bienvenido a esta tienda en linea',
+    productos: productos
+   })
+});
 
 app.listen(port, () => {
-  console.log('Servidor inicializado en http://localhost:' + port);
+    console.log('Servidor inicializado en http://localhost:' + port);
 });
